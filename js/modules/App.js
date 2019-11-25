e0f36ff591465ef68946dda07e2ea2cae354f44e
Miłosz Idzikowski <kontakt@oliweb.pl>
2019-11-25 20:16:07 +0100

    it # Please enter the commit message for your changes. Lines starting

---
+++ js/modules/App.js
@@ -0,0 +1,4 @@
+const player1Name = document.querySelector('input#player1');
+const player2Name = document.querySelector('input#player2');
+const player1Side = document.querySelector('select#player1');
+const player2Side = document.querySelector('select#player2');
---
+++ js/modules/PlayerData.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line no-unused-vars
+class PlayerData {
+    constructor(playerName1, playerName2, choicePlayer1, choicePlayer2) {
+        this.playerName1 = playerName1;
+        this.playerName2 = playerName2;
+        this.choicePlayer1 = choicePlayer1;
+        this.choicePlayer2 = choicePlayer2;
+    }
+
+    getPlayerData(playerNumber) {
+        if (playerNumber == 0) {
+            return [this.playerName1, this.choicePlayer1]
+        }
+        else if (playerNumber == 1) {
+            return [this.playerName2, this.choicePlayer2]
+        }
+        else {
+            console.log('błędne dane')
+        }
+    }
+}
