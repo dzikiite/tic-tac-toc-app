448e8074774b2c9c8d5aee48e596fc75a8ac1bee
dzikiite <kontakt@oliweb.pl>
2019-11-28 15:07:57 +0100

    popup view, handle data

--- js/modules/App.js
+++ js/modules/App.js
@@ -1,4 +1,25 @@
-const player1Name = document.querySelector('input#player1');
-const player2Name = document.querySelector('input#player2');
-const player1Side = document.querySelector('select#player1');
-const player2Side = document.querySelector('select#player2');
+const player1Name = document.querySelector('.form__input--player1');
+const player2Name = document.querySelector('.form__input--player2');
+const player1Side = document.querySelector('.form__select--player1');
+const player2Side = document.querySelector('.form__select--player2');
+const startBtn = document.querySelector('.form__submit-button');
+
+class App {
+    constructor() {
+
+        this.scrollToGame = () => {
+            window.scrollTo({
+                top: window.innerHeight,
+                behavior: 'smooth',
+            });
+        }
+
+        this.fetchPlayerData = () => {
+            const playerData = new PlayerData(player1Name.value, player2Name.value, player1Side.value, player2Side.value);
+        }
+    }
+}
+
+const app = new App();
+
+startBtn.addEventListener('click', app.scrollToGame);
